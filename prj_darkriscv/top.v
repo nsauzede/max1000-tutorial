a7e0f9130b3ca2cae08b500e1163a2050adc393b
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2025-02-17 16:57:52 +0100

    Hardwire spi seq+accelero to leds in top

--- prj_darkriscv/top.v
+++ prj_darkriscv/top.v
@@ -11,6 +11,11 @@ module top #(
         output [7:0] LED,
         inout [8:1] PIO,
 
+        input SEN_SDO,
+        output SEN_SDI,
+        output SEN_CS,
+        output SEN_SPC,
+
         inout [5:0] BDBUS,
         inout [14:0] D
 );
@@ -20,7 +25,7 @@ wire reset;
 wire clk;
 wire [7:0] leds;
 
-assign LED = leds;
+//assign LED = leds;
 assign reset = ~USER_BTN;
 assign rx = BDBUS[0]; // BDBUS[0] is USB UART TX (FPGA RX)
 assign BDBUS[1] = tx; // BDBUS[1] is USB UART RX (FPGA TX)
@@ -42,4 +47,44 @@ endgenerate
         .reset(reset),
         .clk(clk)
     );
+
+wire nrst;
+assign nrst = USER_BTN;
+wire [31:0] spi_mosi_data;
+wire [31:0] spi_miso_data;
+wire [5:0] spi_nbits;
+wire spi_request;
+wire spi_ready;
+
+sequencer U1 (
+        .clk_in(CLK12M),
+        .nrst(nrst),
+
+        .spi_mosi_data(spi_mosi_data),
+        .spi_miso_data(spi_miso_data),
+        .spi_nbits(spi_nbits),
+
+        .spi_request(spi_request),
+        .spi_ready(spi_ready),
+
+        .led_out(LED)
+);
+spi_master U2 (
+        .clk_in(CLK12M),
+        .nrst(nrst),
+
+        .spi_sck(SEN_SPC),
+        .spi_mosi(SEN_SDI),
+        .spi_miso(SEN_SDO),
+        .spi_csn(SEN_CS),
+
+        .mosi_data(spi_mosi_data),
+        .miso_data(spi_miso_data),
+        .nbits(spi_nbits),
+
+        .request(spi_request),
+        .ready(spi_ready)
+);
+
+
 endmodule
